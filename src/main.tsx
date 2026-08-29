1ef292bbe524933364940a0623a76cc841a32f95
amirhamrouni <amiroscrypto2022@gmail.com>
2026-08-30 01:49:02 +0200

    feat: route advanced learning modes through enhanced shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { AssessmentMode, PracticeHub, ReviewMode, SentenceBuilderMode } from './LearningModes';
 import './styles.css';
 import './lesson.css';
 import './twin.css';
+import './modes.css';
+
+function Root() {
+  return (
+    <Routes>
+      <Route path="/practice" element={<PracticeHub />} />
+      <Route path="/review" element={<ReviewMode />} />
+      <Route path="/sentence-builder" element={<SentenceBuilderMode />} />
+      <Route path="/assessment" element={<AssessmentMode />} />
+      <Route path="/*" element={<App />} />
+    </Routes>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 );
