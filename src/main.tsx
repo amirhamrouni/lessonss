023523c7ecbbd15357647995b128ebd57caf8983
amirhamrouni <amiroscrypto2022@gmail.com>
2026-08-30 01:37:20 +0200

    design: load AI Twin motion layer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './lesson.css';
+import './twin.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
