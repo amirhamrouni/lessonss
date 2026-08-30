634aedc4d76d6214f186862fc12fcf33e4eb8143
amirhamrouni <amiroscrypto2022@gmail.com>
2026-08-30 02:04:00 +0200

    feat: activate A1-A2 adaptive home

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import SmartHome from './SmartHome';
+import SmartHomeV2 from './SmartHomeV2';
 import { AssessmentMode, PracticeHub, ReviewMode, SentenceBuilderMode } from './LearningModes';
 import { LearnJourney, UnifiedLessonPlayer } from './LearningPath';
 import './styles.css';
@@ -13,7 +13,7 @@ import './modes.css';
 function Root() {
   return (
     <Routes>
-      <Route path="/" element={<SmartHome />} />
+      <Route path="/" element={<SmartHomeV2 />} />
       <Route path="/learn" element={<LearnJourney />} />
       <Route path="/lesson/:lessonId" element={<UnifiedLessonPlayer />} />
       <Route path="/practice" element={<PracticeHub />} />
