8d492c8246f2bf42e4437cfa6a97625bcd90b093
amirhamrouni <amiroscrypto2022@gmail.com>
2026-09-01 19:19:02 +0200

    Lazy-load English Twin routes to reduce initial bundle

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import SmartHomeV2 from './SmartHomeV2';
-import LearnerSetup from './LearnerSetup';
-import BeginnerFoundation from './BeginnerFoundation';
-import { AssessmentMode, PracticeHub, ReviewMode } from './LearningModes';
-import AdaptiveSentenceBuilder from './AdaptiveSentenceBuilder';
-import { LearnJourney } from './LearningPath';
-import AutoLessonPlayer from './AutoLessonPlayer';
-import TutorMode from './TutorMode';
-import SpeechDrill from './SpeechDrill';
-import VoiceLab from './VoiceLab';
-import { MistakeMemory, ProfileHub } from './ProfileHub';
 import './styles.css';
 import './lesson.css';
 import './twin.css';
@@ -20,25 +8,47 @@ import './modes.css';
 import './language.css';
 import './beginner.css';
 
+const App = React.lazy(() => import('./App'));
+const SmartHomeV2 = React.lazy(() => import('./SmartHomeV2'));
+const LearnerSetup = React.lazy(() => import('./LearnerSetup'));
+const BeginnerFoundation = React.lazy(() => import('./BeginnerFoundation'));
+const AdaptiveSentenceBuilder = React.lazy(() => import('./AdaptiveSentenceBuilder'));
+const AutoLessonPlayer = React.lazy(() => import('./AutoLessonPlayer'));
+const TutorMode = React.lazy(() => import('./TutorMode'));
+const SpeechDrill = React.lazy(() => import('./SpeechDrill'));
+const VoiceLab = React.lazy(() => import('./VoiceLab'));
+const LearnJourney = React.lazy(() => import('./LearningPath').then(module => ({ default: module.LearnJourney })));
+const PracticeHub = React.lazy(() => import('./LearningModes').then(module => ({ default: module.PracticeHub })));
+const ReviewMode = React.lazy(() => import('./LearningModes').then(module => ({ default: module.ReviewMode })));
+const AssessmentMode = React.lazy(() => import('./LearningModes').then(module => ({ default: module.AssessmentMode })));
+const ProfileHub = React.lazy(() => import('./ProfileHub').then(module => ({ default: module.ProfileHub })));
+const MistakeMemory = React.lazy(() => import('./ProfileHub').then(module => ({ default: module.MistakeMemory })));
+
+function RouteFallback() {
+  return <div className="app-shell"><div className="phone"><main className="page"><p>Loading English Twin…</p></main></div></div>;
+}
+
 function Root() {
   return (
-    <Routes>
-      <Route path="/" element={<SmartHomeV2 />} />
-      <Route path="/setup" element={<LearnerSetup />} />
-      <Route path="/start" element={<BeginnerFoundation />} />
-      <Route path="/learn" element={<LearnJourney />} />
-      <Route path="/lesson/:lessonId" element={<AutoLessonPlayer />} />
-      <Route path="/practice" element={<PracticeHub />} />
-      <Route path="/review" element={<ReviewMode />} />
-      <Route path="/sentence-builder" element={<AdaptiveSentenceBuilder />} />
-      <Route path="/assessment" element={<AssessmentMode />} />
-      <Route path="/twin" element={<TutorMode />} />
-      <Route path="/speak" element={<SpeechDrill />} />
-      <Route path="/speak/live" element={<VoiceLab />} />
-      <Route path="/profile" element={<ProfileHub />} />
-      <Route path="/mistakes" element={<MistakeMemory />} />
-      <Route path="/*" element={<App />} />
-    </Routes>
+    <Suspense fallback={<RouteFallback />}>
+      <Routes>
+        <Route path="/" element={<SmartHomeV2 />} />
+        <Route path="/setup" element={<LearnerSetup />} />
+        <Route path="/start" element={<BeginnerFoundation />} />
+        <Route path="/learn" element={<LearnJourney />} />
+        <Route path="/lesson/:lessonId" element={<AutoLessonPlayer />} />
+        <Route path="/practice" element={<PracticeHub />} />
+        <Route path="/review" element={<ReviewMode />} />
+        <Route path="/sentence-builder" element={<AdaptiveSentenceBuilder />} />
+        <Route path="/assessment" element={<AssessmentMode />} />
+        <Route path="/twin" element={<TutorMode />} />
+        <Route path="/speak" element={<SpeechDrill />} />
+        <Route path="/speak/live" element={<VoiceLab />} />
+        <Route path="/profile" element={<ProfileHub />} />
+        <Route path="/mistakes" element={<MistakeMemory />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Suspense>
   );
 }
 
